29ce260aa113e47f28c0d04f96fc386fc9098262
John Lowen David <jdavi977@mtroyal.ca>
2026-01-14 23:32:59 -0700

    Created a manual slider/button for rover movement

--- scripts/view_run.py
+++ scripts/view_run.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.widgets import Slider, Button
 
 def main():
     if len(sys.argv) < 2:
@@ -17,7 +18,9 @@ def main():
 
     fig, ax = plt.subplots()
 
-    margin = 2
+    fig.subplots_adjust(bottom=0.25)
+
+    margin = 0.5
 
     ax.set_xlim(min(x)-margin, max(x)+margin)
     ax.set_ylim(min(y)-margin, max(y)+margin)
@@ -25,18 +28,52 @@ def main():
 
     line, = ax.plot([], [])
     dot, = ax.plot([], [], marker="o")
+    
+    axfreq = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+    freq_slider = Slider(
+        ax=axfreq,
+        label='Slider',
+        valmin=0,
+        valmax=len(x) - 1,
+        valinit=0
+    )
+
+    ax_button = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+    button = Button(ax_button, "Play")
+
+    is_program = False
 
     def update(i):
+        nonlocal is_program
+        is_program = True
         line.set_data(x[:i+1], y[:i+1])
         dot.set_data([x[i]], [y[i]])
+        freq_slider.set_val(i)
+        is_program = False
         return line, dot
-    
+        
+    def changed_slider(i):
+        if is_program == True:
+            return
+        else:
+            anim.pause()
+            i = int(i)
+            line.set_data(x[:i+1], y[:i+1])
+            dot.set_data([x[i]], [y[i]])
+            anim.frame_seq = iter(range(i, len(x)))
+
     anim = animation.FuncAnimation(
         fig,
         update,
         frames=120,
         interval=50,
     )
+
+    freq_slider.on_changed(changed_slider)
+
+    def unpause(event):
+        anim.resume()
+    button.on_clicked(unpause)
 
     writer = animation.PillowWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
     anim.save(run_dir / "animation.gif", writer=writer)
